e124a4171ad306a0c0614f16cf01e9a402c74428
Naveen2706 <naveen271996@gmail.com>
2021-06-04 08:21:57 +0530

    coin flipped

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -1,1 +1,9 @@
 This problem displays winning percentage of Head or Tail Combination in a Singlet, Doublet and Triplet
+
+x=$((RANDOM%2))
+
+if [[ $x -eq 0 ]];then
+	 echo winner : heads
+else 
+	echo winner : tails
+fi
