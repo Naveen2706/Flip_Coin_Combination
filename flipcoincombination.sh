03829d00692313f5e521eb17ddffc8ae5d80c1c2
Naveen2706 <naveen271996@gmail.com>
2021-06-06 06:37:10 +0530

    triplet added and stored

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -3,11 +3,6 @@ This problem displays winning percentage of Head or Tail Combination in a Single
 declare -A Dictionary
 
 loop=0
-heads=0
-tails=0
-headsper=0
-tailsper=0
-sum=0
 coin2=0
 heads2=0
 tails2=0
@@ -16,37 +11,31 @@ while [[ loop -le 10 ]];do
 
 x=$((RANDOM%2))
 y=$((RANDOM%2))
+z=$((RANDOM%2))
 
-if [[ $x -eq 0 ]];then
-			echo winner : heads
-			Dictionary[$loop]="heads"
-   ((heads++))
-   else 
-			echo winner : tailS
-			Dictionary[$loop]="tails" 
-   ((tails++))
-fi
-
-if [[ $y -eq 0 ]] && [[ $x -eq $y ]];then
-                        echo "doublet                      : HH"
-                        doublet[$coin2]="HH"
+if [[ $z -eq 0 ]] && [[ $x -eq $y ]] && [[ $y -eq $z ]];then
+                        echo "triplet                      : HHH"
+                        dictionary[$coin2]="HHH"
    ((coin2++))
    ((heads2++))
-   elif [[ $y -eq 1 ]] && [[ $x -eq $y ]];then
-                        echo "doublet                      : TT"
-                        Dictionary[$coin2]="TT"
+   elif [[ $y -eq 1 ]] && [[ $x -eq $y ]] && [[ $y -eq $z ]];then
+                        echo "triplet                      : TTT"
+                        Dictionary[$coin2]="TTT"
    ((coin2++))
    ((tails2++))
+else 
+echo Other than Triplet
 fi
-   ((loop++))
+
+  ((loop++))
 done
 
-echo Number of doublets -"$coin2"
+echo Number of triplets -"$coin2"
 
-echo heads doublet percentage:
+echo heads triplet percentage:
 			awk "BEGIN {print 100*$heads2/$loop}"
-echo tails doublet percentage:
+echo tails triplet percentage:
 			awk "BEGIN {print 100*$tails2/$loop}"
-echo doublets percentage     :
+echo triplets percentage     :
 			awk "BEGIN {print 100*$coin2/$loop}"
 
